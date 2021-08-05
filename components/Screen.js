abfbabf541cc0d66346e89c9552520296f7d2991
Rebecca Hom <rebecca.hom001@gmail.com>
2021-08-05 13:28:17 -0700

    updated the Screen component so that the width and height match the image

--- components/Screen.js
+++ components/Screen.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Screen = ({ num, src }) => {
-  const [width, setWidth] = useState(800);
-  const [height, setHeight] = useState(600);
-  console.log(src);
+  const [width, setWidth] = useState(300);
+  const [height, setHeight] = useState(300);
+
+  // Gets the image's actual width and height.
+  useEffect(() => {
+    const url = new URL(src);
+    setWidth(url.searchParams.get("w"));
+    setHeight(url.searchParams.get("h"));
+  }, []);
 
   return (<div>
     {src
-      ? <Image src={src} width={width} height={height} alt={`Screen ${num}`}
+      ? <Image src={src} width={width} height={height} alt={`Image ${num}`}
         placeholder="blur"
-        blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-        onResize={() => console.log("Resize images")} />
+        blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUUpCqBwABhQDVMfwd+AAAAABJRU5ErkJggg==" />
       : <p>Image did not load</p>}
   </div>);
 };
