237f317dbfc2cd7fd5241620d55865a2ca0371f3
Rebecca Hom <rebecca.hom001@gmail.com>
2021-08-05 13:09:39 -0700

    added functionality for loading initially randomized images

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,48 @@ import { Container, Grid } from "@material-ui/core";
 import Screen from "../components/Screen";
 
 const HomePage = () => {
-  const randomImage = "https://source.unsplash.com/random";
+  const initialImages = 32;
+  const minW = 30;
+  const minH = 30;
+  const maxW = 1200;
+  const maxH = 800;
+
   const [images, setImages] = useState([]);
   const [screenList, setScreenList] = useState([]);
 
+  /**
+   * Randomly generates a number from a range
+   * @param {Number} min - Minimum number for the range
+   * @param {Number} max - Maximum number for the range
+   * @returns a random number in between a range
+   */
+  const random = (min, max) => Math.floor((Math.random() * (max -min)) + min);
+
+  /**
+   *Randomly generates an image.
+   * @returns an image URL
+   */
+  const getImage = async () => {
+    //  Fetches the image url with random width and height.
+    const image = await fetch(`https://source.unsplash.com/random/${random(minW, maxW)}x${random(minH, maxH)}`);
+
+    return image.url;
+  };
+
   useEffect(() => {
-    const getImage = async () => {
-      const image = await fetch(randomImage);
-      console.log(image.url);
-      setImages(images => [...images, image.url]);
-      console.log(images);
+    //  Loads an initial images.
+    const loadImages = async () => {
+      let arr = images;
+
+      while (arr.length < initialImages) {
+        const image = await getImage();
+        arr.push(image);
+        arr = Array.from(new Set(arr));
+        setImages(arr);
+      }
     }
 
-    getImage();
+    loadImages();
   }, []);
 
   return <Container>
