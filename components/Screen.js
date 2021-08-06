e1d0a2000e9517c09ef28ab22dead4db10a8fc9a
Rebecca Hom <rebecca.hom001@gmail.com>
2021-08-05 21:56:02 -0700

    added class name for styling

--- components/Screen.js
+++ components/Screen.js
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
-const Screen = ({ num, src }) => {
+/**
+ * Creates a resizable image screen.
+ * @param {Number} num - Index of the image in the array
+ * @param {String} src - Image URL
+ * @param {Boolean} screen - Checks if the image is a screen or a regular image
+ * @returns
+ */
+const Screen = ({ num, src, screen = true }) => {
   const [width, setWidth] = useState(300);
   const [height, setHeight] = useState(300);
 
-  // Gets the image's actual width and height.
+  // Gets the image's actual width and height if it's not in the gallery.
   useEffect(() => {
-    const url = new URL(src);
-    setWidth(url.searchParams.get("w"));
-    setHeight(url.searchParams.get("h"));
+    if (screen) {
+      const url = new URL(src);
+      setWidth(url.searchParams.get("w"));
+      setHeight(url.searchParams.get("h"));
+    }
   }, []);
 
   return (<div>
     {src
       ? <Image src={src} width={width} height={height} alt={`Image ${num}`}
-        placeholder="blur"
+        placeholder="blur" className="screen"
         blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUUpCqBwABhQDVMfwd+AAAAABJRU5ErkJggg==" />
       : <p>Image did not load</p>}
   </div>);
